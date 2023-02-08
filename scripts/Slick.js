74467decf65b0a98195560db5b8f40e6299fac87
viniciusmaculo <vinimaculo@gmail.com>
2023-02-08 10:59:42 -0300

    Sobe projeto final da disciplina para o GitHub

---
+++ scripts/Slick.js
@@ -0,0 +1,11 @@
+$(".slider__container").slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
